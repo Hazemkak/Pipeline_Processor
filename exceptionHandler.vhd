e710aa94f8a61242d4ac3aeaa8e7da9a60355905
hazemkak <hazemkasim3@gmail.com>
2022-01-06 15:45:03 +0200

    chnaging in exception handler code

--- exceptionHandler.vhd
+++ exceptionHandler.vhd
@@ -21,56 +21,21 @@ END ENTITY ExceptionHandler;
 ARCHITECTURE ExceptionHandlerArc OF ExceptionHandler IS
 BEGIN
 
-		PROCESS(clk) IS
-		BEGIN
-			IF rising_edge(clk) THEN  
 
-                IF rst = '1' THEN
-
-                    memoAddress <= "00000000000000000000000000000001";--32bit zeros
-                    enableInReg <= '0';
-
-				ELSIF SP_select(0) = '1' THEN -- check for empty stack pop
-
-                    IF SP_select(1)='1' and SP_select(2)='0' THEN -- 01 increment by 1 POP16 , so check if SP_data is at last memo place
-
-                        IF SP_data > "00000000000011111111111111111110" THEN
-                            memoAddress <= "00000000000000000000000000000010";
-                            enableInReg <= '1';
-                        ELSE 
-                            enableInReg <= '0';
-                            memoAddress <= SP_data;
-                        END IF;
-
-                    ELSIF SP_select(1)='1' and SP_select(2)='1' THEN -- 11 increment by 2 POP32 , so check if SP_data is at  prev last memo place
-
-                        IF SP_data > "00000000000011111111111111111101" THEN
-                            memoAddress <= "00000000000000000000000000000011";
-                            enableInReg <= '1';
-                        ELSE 
-                            enableInReg <= '0';
-                            memoAddress <= SP_data;
-                        END IF;
-                        
-                    END IF;
-                    
-                ELSE
-                    
-                    IF MR = '1' or MW16 = '1' or MW32 = '1' THEN
-                        
-                        IF aluData > "00000000000011111111111111111111" THEN
-                            memoAddress <= "00000000000000000000000000000101";
-                            enableInReg <= '1';
-                        ELSE
-                            memoAddress <= aluData;
-                            enableInReg <= '0';
-                        END IF;
-                        
-                    END IF;
-
-				END IF;
-
-			END IF;
-		END PROCESS;
+        memoAddress <= "00000000000000000000000000000001" WHEN rst='1' 
+            ELSE "00000000000000000000000000000011" WHEN (SP_select(0) = '1' AND SP_select(1)='1' AND SP_select(2)='0' AND SP_data > "00000000000011111111111111111110") --inc 1
+            ELSE "00000000000000000000000000000011" WHEN (SP_select(0) = '1' AND SP_select(1)='1' AND SP_select(2)='1' AND SP_data > "00000000000011111111111111111101") --inc 2
+            ELSE SP_data WHEN SP_select(0) = '1' 
+            ELSE "00000000000000000000000000000101" WHEN ((MR = '1' or MW16 = '1' or MW32 = '1') AND aluData > "00000000000011111111111111111111") 
+            ELSE aluData WHEN (MR = '1' or MW16 = '1' or MW32 = '1')
+            ELSE "00000000000000000000000000001111";--put anything what will be read won't be used
+        
+        enableInReg <= '0' WHEN rst='1'
+            ELSE '1' WHEN (SP_select(0) = '1' AND SP_select(1)='1' AND SP_select(2)='0' AND SP_data > "00000000000011111111111111111110") 
+            ELSE '1' WHEN (SP_select(0) = '1' AND SP_select(1)='1' AND SP_select(2)='1' AND SP_data > "00000000000011111111111111111101") 
+            ELSE '0' WHEN SP_select(0) = '1' 
+            ELSE '1' WHEN ((MR = '1' or MW16 = '1' or MW32 = '1') AND aluData > "00000000000011111111111111111111") 
+            ELSE '0' WHEN (MR = '1' or MW16 = '1' or MW32 = '1')
+            ELSE '0';
 
 END ExceptionHandlerArc;
